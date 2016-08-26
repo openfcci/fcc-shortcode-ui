d5d76327220ecee4785388d3ae309bf43e7d7f8f
Ryan Veitch <ryan.r.veitch@gmail.com>
2016-08-26 17:22:40 -0500

    Jw update (#6)
    * Imported changes
    
    * cleaned code
    
    * wip
    
    * resolved console error

--- fcc-shortcode-ui.php
+++ fcc-shortcode-ui.php
@@ -1,35 +1,31 @@
 <?php
 /**
  * Plugin Name: FCC Shortcode UI
- * Version: 0.15.12.16
- * Description: Adds 'Insert Post Element' visual UI to shortcode embeds
- * Author: FCC Digital / Ryan Veitch
- * Author URI: http://forumcomm.com/
+ * Description: Adds 'Insert Post Element' visual UI to shortcode embeds. Includes: JW Player video embed.
+ * Plugin URI:  https://github.com/openfcci/fcc-shortcode-ui
+ * Author:      Forum Communications Company
+ * Author URI:  http://www.forumcomm.com/
+ * License:     GPL v2 or later
  * Text Domain: shortcode-ui
- * License: GPL v2 or later
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Version:     1.16.05.18
  */
 
- /* DOCUMENTATION: https://github.com/wp-shortcake/shortcake/wiki/Registering-Shortcode-UI
-  * Available Shortcode UI attribute fields include:
-  * Text, checkbox, textarea, radio, select, email, url, number, date, attachment, color, post_select.
-  */
+/*
+ DOCUMENTATION: https://github.com/wp-shortcake/shortcake/wiki/Registering-Shortcode-UI
+ Available Shortcode UI attribute fields include:
+ Text, checkbox, textarea, radio, select, email, url, number, date, attachment, color, post_select.
+*/
+
+/*--------------------------------------------------------------
+# Admin Notices
+--------------------------------------------------------------*/
 
 /**
  * If Shortcake isn't active, then this demo plugin doesn't work either
  */
 add_action( 'init', 'fcc_shortcode_ui_detection' );
 function fcc_shortcode_ui_detection() {
-	if ( !function_exists( 'shortcode_ui_register_for_shortcode' ) ) {
+	if ( ! function_exists( 'shortcode_ui_register_for_shortcode' ) ) {
 		add_action( 'admin_notices', 'fcc_shortcode_ui_notices' );
 	}
 }
@@ -40,115 +36,25 @@ function fcc_shortcode_ui_notices() {
 	}
 }
 
-/**
- * Register the two shortcodes independently of their UI.
- * Shortcodes should always be registered, but shortcode UI should only
- * be registered when Shortcake is active.
- */
+/*--------------------------------------------------------------
+# Register Shortcodes
+--------------------------------------------------------------*/
+
 function shortcode_ui_fcc_register_shortcodes() {
-
-	// JW Player Embed
 	add_shortcode( 'fcc_jw_player', 'fcc_shortcode_jw_player' );
-
-	// This is a simple example for a pullquote with a citation.
-	add_shortcode( 'shortcake_dev', 'shortcode_ui_dev_shortcode' );
-
 }
 add_action( 'init', 'shortcode_ui_fcc_register_shortcodes' );
 
+/*--------------------------------------------------------------
+# JW Player Media Embed
+--------------------------------------------------------------*/
 
 /**
- * An example shortcode with many editable attributes (and more complex UI)
- */
-function shortcode_ui_dev_advanced_example() {
-
-	/**
-	 * Register UI for your shortcode
-	 *
-	 * @param string $shortcode_tag
-	 * @param array $ui_args
-	 */
-	shortcode_ui_register_for_shortcode( 'shortcake_dev',
-		array(
-			/*
-			 * How the shortcode should be labeled in the UI. Required argument.
-			 */
-			'label' => esc_html__( 'Shortcake Dev', 'shortcode-ui' ),
-			/*
-			 * Include an icon with your shortcode. Optional.
-			 * Use a dashicon, or full URL to image.
-			 */
-			'listItemImage' => 'dashicons-editor-quote',
-			/*
-			 * Limit this shortcode UI to specific posts. Optional.
-			 */
-			'post_type' => array( 'post' ),
-			/*
-			 * Register UI for the "inner content" of the shortcode. Optional.
-			 * If no UI is registered for the inner content, then any inner content
-			 * data present will be backed up during editing.
-			 */
-			'inner_content' => array(
-				'label'        => esc_html__( 'Quote', 'shortcode-ui' ),
-				'description'  => esc_html__( 'Include a statement from someone famous.', 'shortcode-ui' ),
-			),
-			/*
-			 * Register UI for attributes of the shortcode. Optional.
-			 *
-			 * If no UI is registered for an attribute, then the attribute will
-			 * not be editable through Shortcake's UI. However, the value of any
-			 * unregistered attributes will be preserved when editing.
-			 *
-			 * Each array must include 'attr', 'type', and 'label'.
-			 * 'attr' should be the name of the attribute.
-			 * 'type' options include: text, checkbox, textarea, radio, select, email,
-			 *     url, number, and date, post_select, attachment, color.
-			 * Use 'meta' to add arbitrary attributes to the HTML of the field.
-			 * Use 'encode' to encode attribute data. Requires customization to callback to decode.
-			 * Depending on 'type', additional arguments may be available.
-			 */
-			'attrs' => array(
-				array(
-					'label'       => esc_html__( 'Attachment', 'shortcode-ui' ),
-					'attr'        => 'attachment',
-					'type'        => 'attachment',
-					/*
-					 * These arguments are passed to the instantiation of the media library:
-					 * 'libraryType' - Type of media to make available.
-					 * 'addButton' - Text for the button to open media library.
-					 * 'frameTitle' - Title for the modal UI once the library is open.
-					 */
-					'libraryType' => array( 'image' ),
-					'addButton'   => esc_html__( 'Select Image', 'shortcode-ui' ),
-					'frameTitle'  => esc_html__( 'Select Image', 'shortcode-ui ' ),
-				),
-				array(
-					'label'  => esc_html__( 'Citation Source', 'shortcode-ui' ),
-					'attr'   => 'source',
-					'type'   => 'text',
-					'encode' => true,
-					'meta'   => array(
-						'placeholder' => esc_html__( 'Test placeholder', 'shortcode-ui' ),
-						'data-test'   => 1,
-					),
-				),
-				array(
-					'label' => esc_html__( 'Select Page', 'shortcode-ui' ),
-					'attr' => 'page',
-					'type' => 'post_select',
-					'query' => array( 'post_type' => 'page' ),
-					'multiple' => true,
-				),
-			),
-		)
-	);
-}
-//add_action( 'register_shortcode_ui', 'shortcode_ui_dev_advanced_example' );
-
-
-
-/**
- * JW Player Media Embed
+ * JW Player Video Embed - UI Functions
+ *
+ * @author Ryan Veitch <ryan.veitch@forumcomm.com>
+ * @since 1.16.02.23
+ * @version 1.16.05.18
  */
 function fcc_shortcode_ui_jw_player() {
 
@@ -163,31 +69,8 @@ function fcc_shortcode_ui_jw_player() {
 	 */
 	shortcode_ui_register_for_shortcode( 'fcc_jw_player',
 		array(
-			/*
-			 * How the shortcode should be labeled in the UI. Required argument.
-			 */
 			'label' => esc_html__( 'JW Player Media Embed', 'shortcode-ui' ),
-			/*
-			 * Include an icon with your shortcode. Optional.
-			 * Use a dashicon, or full URL to image.
-			 */
 			'listItemImage' => $jwlogo,
-
-			/*
-			 * Register UI for attributes of the shortcode. Optional.
-			 *
-			 * If no UI is registered for an attribute, then the attribute will
-			 * not be editable through Shortcake's UI. However, the value of any
-			 * unregistered attributes will be preserved when editing.
-			 *
-			 * Each array must include 'attr', 'type', and 'label'.
-			 * 'attr' should be the name of the attribute.
-			 * 'type' options include: text, checkbox, textarea, radio, select, email,
-			 *     url, number, and date, post_select, attachment, color.
-			 * Use 'meta' to add arbitrary attributes to the HTML of the field.
-			 * Use 'encode' to encode attribute data. Requires customization to callback to decode.
-			 * Depending on 'type', additional arguments may be available.
-			 */
 			'attrs' => array(
 				array(
 					'label'  => esc_html__( 'JW Player Video Key', 'shortcode-ui' ),
@@ -200,96 +83,69 @@ function fcc_shortcode_ui_jw_player() {
 						'data-test'   => 1,
 					),
 				),
-        array(
-          'label'       => esc_html__( 'Disable Autostart?', 'shortcode-ui' ),
-          'description' => 'Select this on any video after the first when embedding more than one video in a post to prevent multiple videos
-          playing at the same time.',
-          'attr'        => 'disable_autostart',
-          'type'        => 'checkbox',
-        ),
+				array(
+					'label'       => esc_html__( 'Disable Autostart?', 'shortcode-ui' ),
+					'description' => 'Select this on any video after the first when embedding more than one video in a post to prevent multiple videos
+					playing at the same time.',
+					'attr'        => 'disable_autostart',
+					'type'        => 'checkbox',
+				),
 			),
 		)
 	);
 }
 add_action( 'register_shortcode_ui', 'fcc_shortcode_ui_jw_player' );
 
-
 /**
- * Render the shortcode based on supplied attributes
- */
-function shortcode_ui_dev_shortcode( $attr, $content = '', $shortcode_tag ) {
-
-	$attr = shortcode_atts( array(
-		'source'     => '',
-		'attachment' => 0,
-		'source'     => null,
-	), $attr, $shortcode_tag );
-
-	ob_start();
-
-	?>
-
-	<section class="pullquote" style="padding: 20px; background: rgba(0,0,0,0.1);">
-		<p style="margin:0; padding: 0;">
-		<b>Content:</b> <?php echo wpautop( wp_kses_post( $content ) ); ?></br>
-		<b>Source:</b> <?php echo wp_kses_post( $attr[ 'source' ] ); ?></br>
-		<b>Image:</b> <?php echo wp_kses_post( wp_get_attachment_image( $attr[ 'attachment' ], array( 50, 50 ) ) ); ?></br>
-		</p>
-	</section>
-
-	<?php
-
-	return ob_get_clean();
-
-}
-
-/**
- * Render the shortcode based on supplied attributes
+ * JW Player Video Embed (Callback Function)
+ *
+ * @author Ryan Veitch <ryan.veitch@forumcomm.com>
+ * @since 1.16.02.23
+ * @version 1.16.08.26
  */
 function fcc_shortcode_jw_player( $attr, $content = '', $shortcode_tag ) {
 
-	$attr = shortcode_atts( array(
-		'key'     => '',
-		'attachment' => 0,
-		'key'     => null,
-    'disable_autostart' => 0
-	), $attr, $shortcode_tag );
+	 	$attr = shortcode_atts( array(
+	 		'key'               => '',
+	 		'attachment'        => 0,
+	 		'key'               => null,
+			'disable_autostart' => '',
+	 	), $attr, $shortcode_tag );
 
-	ob_start();
+	 	ob_start(); // Start Output Buffer
 
-	if ( is_admin() ) { echo '<div align="center" class="fccjwplayer" style="max-width: 650px;">'; }
+		$video_key = wp_kses_post( $attr['key'] );
+		$player_key = 'XmRneLwC'; // TODO Make key a dynamic field?
+		if ( 'true' == $attr['disable_autostart'] ) {
+			$autostart = 'false'; // Disable Autostart
+		} else {
+			$autostart = 'true'; // Enable Autostart
+		}
 
-	?>
+	 	if ( is_admin() ) { echo '<div align="center" class="fccjwplayer" style="max-width: 650px;">'; }
+		echo '
+	   <script src="https://content.jwplatform.com/libraries/'.$player_key.'.js"></script>
+	   <div id="'.$video_key.'">Loading the player...</div>
+	   <script type="text/javascript">
+	   var playerInstance = jwplayer("'.$video_key.'");
+	   playerInstance.setup({
+	   	file: "https://content.jwplatform.com/videos/'.$video_key.'.mp4",
+	   	image: "https://assets-jpcust.jwpsrv.com/thumbs/'.$video_key.'.jpg",
+	   	autostart: '.$autostart.',
+	   });
+		 console.log("JW Player Mode: " + playerInstance.getRenderingMode());
+	   </script>
+	   ';
+	 	if ( is_admin() ) { echo '</div>'; }
 
-    <script type="text/javascript" src="//content.jwplatform.com/libraries/XmRneLwC.js"></script>
+	 	return ob_get_clean(); // End Output Buffer
+}
 
-    <div id="<?php echo wp_kses_post( $attr[ 'key' ] ); ?>">Loading the player...</div>
+/*--------------------------------------------------------------
+# SCRIBBLE LIVE
+--------------------------------------------------------------*/
 
-    <script type="text/javascript">
-      var playerInstance = jwplayer("<?php echo wp_kses_post( $attr[ 'key' ] ); ?>");
-      playerInstance.setup({
-      	file: "//content.jwplatform.com/videos/<?php echo wp_kses_post( $attr[ 'key' ] ); ?>.mp4",
-      	image: "http://assets-jpcust.jwpsrv.com/thumbs/<?php echo wp_kses_post( $attr[ 'key' ] ); ?>.jpg",
-        <?php
-          //If Disable Autostart is checked, Don't Autostart the video
-          if($attr[ 'disable_autostart' ]){
-            echo "autostart: false";
-          }else{
-            echo "autostart: true";
-          }
-        ?>
-      });
-    </script>
-
-  <?php
-
-	if ( is_admin() ) { echo '</div>'; }
-
-	return ob_get_clean();
-
-} // End function fcc_shortcode_jw_player
-
-/* SCRIBBLE LIVE FORMAT
+/* SCRIBBLE LIVE FORMAT (For Later Use)
 
 <div class="scrbbl-embed" data-src="/event/1717426"></div>
 
@@ -305,4 +161,3 @@ function fcc_shortcode_jw_player( $attr, $content = '', $shortcode_tag ) {
 </script>
 
 */
-//Register Settings
